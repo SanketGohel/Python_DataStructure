b5a46b7cb89fb1b43c616ba9d583dbc7a8e88e1c
Sanket Gohel <43456045+SanketGohel@users.noreply.github.com>
2020-06-06 00:07:03 -0400

    RecentCounter using Queue

---
+++ RecentCounter.py
@@ -0,0 +1,34 @@
+#Write a class RecentCounter to count recent requests.
+
+#It has only one method: ping(int t), where t represents some time in milliseconds.
+
+#Return the number of pings that have been made from 3000 milliseconds ago until now.
+
+#Any ping with time in [t - 3000, t] will count, including the current ping.
+
+#It is guaranteed that every call to ping uses a strictly larger value of t than before.
+
+
+
+class RecentCounter:
+
+    def __init__(self):
+        self.queue =[]
+        self.count = 0
+        
+
+    def ping(self, t: int) -> int:
+        self.queue.append(t)
+        self.count += 1
+        while self.queue[0] < (t-3000):
+            self.queue.pop(0)
+            self.count -= 1
+        return self.count
+            
+    
+        
+
+
+# Your RecentCounter object will be instantiated and called as such:
+# obj = RecentCounter()
+# param_1 = obj.ping(t)
